d26a89479e2d27dc206bfbfeb6b68fa79e78052e
MihaiAlexandru333 <durduvsmihai@yahoo.com>
2023-06-16 10:46:42 +0300

    add dots to die faces

--- src/Die.jsx
+++ src/Die.jsx
@@ -2,18 +2,15 @@
 
 
 
-export default function Die(props) {
-    console.log(props)
-    const {value, isHeld} = props
-    const styles = {backgroundColor: props.isHeld ? "#59E391" : "white"}
-
-
+export default function Die({ value, holdDice, isHeld }) {
+    const dots = [];
+    for (let i = 0; i < value; i++) {
+      dots.push(<div key={i} className="dot"></div>);
+    }
+  
     return (
-        <div className="die-face" 
-        style={styles}
-        onClick={props.holdDice}
-        >
-            <h2 className="die-num">{props.value}</h2>
-        </div>
+      <div className={`die ${isHeld ? 'is-held' : ''}`} onClick={holdDice}>
+        <div className="dots-container">{dots}</div>
+      </div>
     )
-}
+  }
